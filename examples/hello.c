632ccf738bc0e7881f1a6189d91f42ddb6f1f79f
Andrei Kholodnyi <Andrei.Kholodnyi@windriver.com>
2020-06-18 21:28:44 +0200

    add comments

--- examples/hello.c
+++ examples/hello.c
@@ -4,6 +4,10 @@
 int main() {
 	struct utsname data;
 
+	/* get name and information about current kernel */
+	/* sysname[]; - Operating system name (e.g., "Linux") */
+	/* machine[]; - Hardware identifier */
+
 	uname(&data);
 	printf("Hello, %s %s\n", data.sysname, data.machine);
 	return 0;
